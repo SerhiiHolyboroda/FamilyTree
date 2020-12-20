1340564ed861fe05b3fe4e719ddb69b6626de6ef
Serhii Holyboroda <49484600+SerhiiHolyboroda@users.noreply.github.com>
2020-12-20 21:42:16 +0200

    Adding files

---
+++ src/main/kotlin/Person.kt
@@ -0,0 +1,33 @@
+class Person(
+    var firstName: String,
+    var lastName: String,
+    val age: Int,
+    val Mother: Person?,
+    val Father: Person?,
+    var Sisters: ArrayList<Person>?,
+    var Brothers: ArrayList<Person>?
+){
+//    var noOfRelatives = 0
+//    fun counter(): Int {
+//        return noOfRelatives
+//    }
+//    init {
+//        noOfRelatives++
+//    }
+
+    companion object {
+        var noOfRelatives: Int = 0
+
+        fun counter(): Int {
+            return noOfRelatives
+        }
+    }
+
+    init {
+        noOfRelatives++
+    }
+    fun getRelatives(){
+        println(noOfRelatives)
+    }
+}
+
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,29 @@
+fun main(args: Array<String>) {
+    var myGrandmaFather = Person("Тимофій", "Васильчук", 117,null  , null,null,null)
+    var myGrandmaMother = Person("Оксана", "Ковалець", 126,null  , null,null,null)
+    var myGrandpaFather = Person("Сергій", "Гнатюк", 142,null  , null,null,null)
+    var myGrandpaMother = Person("Олена", "Леськів", 120,null  , null,null,null)
+
+//    M - дідусь і бабуся по маминій лінії
+//    F - дідусь і бабуся по татовій лінії
+    var myMGrandma = Person("Ольга", "Гнатюк", 86,myGrandmaFather  , myGrandmaMother,null,null)
+    var myMGrandpa = Person("Роман", "Гнатюк", 87,myGrandpaFather  , myGrandpaMother,null,null)
+
+    var myFGrandma = Person("Єлизавета", "Леськів", 88,null  , null,null,null)
+    var myFGrandpa = Person("Роман", "Леськів", 90,null  , null,null,null)
+
+    var myMother = Person("Леся", "Леськів", 45,myMGrandma  , myMGrandpa ,null,null)
+    var myFather = Person("Олег", "Леськів", 50,myFGrandma  , myFGrandpa,null,null)
+
+
+    var mySelf = Person("Олег(Другий)", "Леськів", 50, myMother , myFather ,  null,null)
+
+
+
+    println( myFather.getRelatives())
+    fun getMe(){
+        println(mySelf.firstName)
+        println(mySelf.lastName)
+    }
+    getMe()
+}
